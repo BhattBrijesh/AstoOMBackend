961191c561d93f860678595c9b71346500d18cca
Brijesh Bhatt <brijesh.bhatt@method-hub.com>
2025-07-20 14:48:40 +0530

    changes for vercel deployment

---
+++ api/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const { handleDbConnection } = require("../connection");
+const { contactUsRoutes } = require("../routes/contactUs.routes");
+const app = express();
+
+// env variables
+const PORT = 3000;
+
+const dbConnectUrl = process.env.MONGOOSE_CONNECTION_URL;
+
+// middleware body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// DB Connection
+handleDbConnection(dbConnectUrl);
+
+// routes
+app.use("/api", contactUsRoutes);
+
+//server
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
